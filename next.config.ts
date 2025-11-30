1e5be2882700dc6aca5a9b34b278535d1a3e8ca8
Joy-sameza <samgaming500@gmail.com>
2025-11-30 19:43:56 +0100

    chore: add pnpm workspace configuration and update TypeScript settings
    - Added pnpm-workspace.yaml to ignore built dependencies for sharp.
    - Reformatted tsconfig.json for better readability and updated include paths to include build types.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: "build",
+  images: {
+    domains: ["cdn.mybrainboostapp.com", "cdn-staging.mybrainboostapp.com"],
+  },
 };
 
 export default nextConfig;
