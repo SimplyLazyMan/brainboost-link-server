d4edc89b58c228b2f716c7455079470b3227cf5a
Joy-sameza <samgaming500@gmail.com>
2025-11-30 14:17:12 +0100

    Add configuration files and update project settings for Next.js and Firebase

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "build",
-  images: {
-    domains: ["cdn.mybrainboostapp.com", "cdn-staging.mybrainboostapp.com"],
-  }
 };
 
 export default nextConfig;
