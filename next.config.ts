577c155541fcbaeca33cfa37cc4fff87f6f9ca9b
Joy-sameza <samgaming500@gmail.com>
2025-12-08 11:21:59 +0100

    feat: update pnpm workspace and TypeScript configuration
    - Added `onlyBuiltDependencies` for `esbuild` and `workerd` in pnpm-workspace.yaml.
    - Introduced a new _headers file for caching static assets in Cloudflare Workers.
    - Updated TypeScript target to `es2024` and included additional type definitions.
    - Created a new wrangler configuration file for the Cloudflare Worker with asset and durable object bindings.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "build",
+  // distDir: "build",
   images: {
     domains: ["cdn.mybrainboostapp.com", "cdn-staging.mybrainboostapp.com"],
   },
